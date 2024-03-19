65153be03b65753c2b1bc2e1d840dc67c16d548a
Siva-21-Developer <siva2122001@gmail.com>
2024-03-19 12:41:50 +0530

    19/03 update

--- force-app/main/default/lwc/accDetails/accDetails.js
+++ force-app/main/default/lwc/accDetails/accDetails.js
@@ -2,7 +2,6 @@ import { LightningElement,wire } from 'lwc';
 import NAME_FIELD  from "@salesforce/schema/Account.Name";
 import REVENU_FIELD  from "@salesforce/schema/Account.AnnualRevenue";
 import INDUSTRY_FIELD from "@salesforce/schema/Account.Industry";
-
 import getAccountsData from "@salesforce/apex/LightTest.methodName";
 import getAccountCount from '@salesforce/apex/LightTest.getAccountCount';
 import getClientCount from '@salesforce/apex/LightTest.ClientCount';
@@ -16,7 +15,8 @@ export default class AccDetails extends LightningElement {
     ClientCount;
 
     @wire(getAccountCount)
-    wiredAccountCount({ error, data }) {
+    wiredAccountCount({ error, data }) 
+    {
         if (data) {
             this.ProjectCount = data;
         } else if (error) {
@@ -25,7 +25,8 @@ export default class AccDetails extends LightningElement {
     }
 
     @wire(getClientCount)
-    wiredClientCount({ error, data }) {
+    wiredClientCount({ error, data }) 
+    {
         if (data) {
             this.ClientCount = data;
         } else if (error) {
